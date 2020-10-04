b885ec7bf14935e3368a0815a22481db53d5782a
Nandhu Krishnan <nandhukrishnan96@gmail.com>
2020-10-04 10:54:02 +0530

    added type definitions

---
+++ include/Definitions.h
@@ -0,0 +1,14 @@
+//Project: Library Management System
+//File: Definitions.h  
+//Author: Nandhu Krishnan
+
+#ifndef DEFINITIONS_H
+#define DEFINITIONS_H
+
+#include <iostream>
+
+using STRING = std::string;
+using UINT = uint32_t;
+using TIME = time_t;
+
+#endif
